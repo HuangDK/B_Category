a38853bccd02ea704b25626591804af5d3176d2b
huangdk <huangdk1707@gmail.com>
2020-10-16 10:34:08 +0800

    first commit

---
+++ B_Category/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  B_Category
+//
+//  Created by hdk on 2020/10/16.
+//  Copyright © 2020 hdk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ B_Category/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  B_Category
+//
+//  Created by hdk on 2020/10/16.
+//  Copyright © 2020 hdk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
