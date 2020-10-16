f047d9cf1d7e373644a1ca136b31a11f93c8101b
huangdk <huangdk1707@gmail.com>
2020-10-16 16:15:18 +0800

    update code

---
+++ B_Category/B_Category/CTMediator+B.h
@@ -0,0 +1,20 @@
+//
+//  CTMediator+B.h
+//  B_Category
+//
+//  Created by hdk on 2020/10/16.
+//  Copyright © 2020 hdk. All rights reserved.
+//
+
+#import <CTMediator/CTMediator.h>
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CTMediator (B)
+
+- (UIViewController *)B_viewControllerWithContentText:(NSString *)contentText;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ B_Category/B_Category/CTMediator+B.m
@@ -0,0 +1,19 @@
+//
+//  CTMediator+B.m
+//  B_Category
+//
+//  Created by hdk on 2020/10/16.
+//  Copyright © 2020 hdk. All rights reserved.
+//
+
+#import "CTMediator+B.h"
+
+@implementation CTMediator (B)
+
+- (UIViewController *)B_viewControllerWithContentText:(NSString *)contentText {
+    NSMutableDictionary *params = [[NSMutableDictionary alloc] init];
+    params[@"contentText"] = contentText;
+    return [self performTarget:@"B" action:@"viewController" params:params shouldCacheTarget:NO];
+}
+
+@end
